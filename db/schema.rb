4dd687c4f64022291d6662d5737e4f71b8e4d1a6
sander souto <sander.soares@yahoo.com.br>
2020-11-27 14:24:56 -0300

    Yelp MVP challenge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_163407) do
+ActiveRecord::Schema.define(version: 2020_11_27_170628) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 2020_11_27_163407) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
